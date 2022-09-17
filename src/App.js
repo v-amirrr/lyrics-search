649bc1ce8e275e81d12c0fde8c6e901c521e08e0
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-09-17 22:14:02 +0430

    feat: added a background to the project and also added the routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
 
+import { Routes, Route, useLocation } from 'react-router-dom';
+
 import HomePage from './components/HomePage';
 
+import styled from 'styled-components';
 import { AnimatePresence } from 'framer-motion';
 
 const App = () => {
+
+    const location = useLocation();
+
     return (
         <>
+            <Background><img src='/images/bg.jpg' alt="background" /></Background>
             <AnimatePresence>
-                <HomePage />
+                <Routes location={location} key={location.key}>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
             </AnimatePresence>
         </>
     );
 };
+
+const Background = styled.div`
+    position: absolute;
+    inset: 0 0 0 0;
+    z-index: -9;
+    
+    img {
+        filter: blur(5px);
+        width: 100%;
+        height: 100%;
+        z-index: -10;
+    }
+`;
 
 export default App;
