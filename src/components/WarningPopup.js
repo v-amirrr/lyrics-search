b2b1db7817c73e4fec58e1b805e54fbcb75ae3f1
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-12 22:55:09 +0330

    perf: used BEM for naming classes and replaced Styled-Components with SCSS in WarningPopup component

--- src/components/WarningPopup.js
+++ src/components/WarningPopup.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
-import styled from 'styled-components';
+import "./WarningPopup.scss"
 import { motion, AnimatePresence } from 'framer-motion';
 
 const popUpPageVariants = {
     hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 0.2, type: 'tween', when: "beforeChildren" } },
-    exit: { opacity: 0, transition: { duration: 0.2, type: 'tween', when: "afterChildren" } }
+    visible: { opacity: 1, transition: { duration: 0.4, type: 'tween', when: "beforeChildren" } },
+    exit: { opacity: 0, transition: { duration: 0.4, type: 'tween', when: "afterChildren" } }
 };
 
 const popUpContainerVariants = {
@@ -44,115 +44,119 @@ const WarningPopup = () => {
                 {
                     warningPopup
                     &&
-                    <PopupPage initial='hidden' animate='visible' exit='exit' variants={popUpPageVariants} onKeyUp={e => e.key === "Enter" && warningPopUpSubmitHandler}>
-                        <motion.div className="popup-container" variants={popUpContainerVariants}>
-                            <motion.h1 variants={popUpItemVariants} className="popup-title">things you need to know</motion.h1>
-                            <motion.p variants={popUpItemVariants} className="popup-warning">If you're in sanctioned countries like <b>Iran</b>, you have to turn on your <b>VPN</b> for using the app.</motion.p>
-                            <motion.p variants={popUpItemVariants} className="popup-text">
+                    <motion.section className='warning-popup' initial='hidden' animate='visible' exit='exit' variants={popUpPageVariants} onKeyUp={e => e.key === "Enter" && warningPopUpSubmitHandler}>
+                        <motion.div className="warning-popup__container" variants={popUpContainerVariants}>
+                            <motion.h1 className="warning-popup__title" variants={popUpItemVariants}>
+                                things you need to know
+                            </motion.h1>
+                            <motion.p className="warning-popup__warning-text" variants={popUpItemVariants}>
+                                If you're in sanctioned countries like <b>Iran</b>, you have to turn on your <b>VPN</b> for using the app.
+                            </motion.p>
+                            <motion.p className="warning-popup__description-text" variants={popUpItemVariants}>
                                 In this app you can search for any song you want and then if you click on them, you'll see more information about that song, such as artist, album, genre, lyrics and etc.
                             </motion.p>
-                            <motion.button autoFocus variants={popUpItemVariants} type="submit" className="popup-button" onClick={warningPopUpSubmitHandler}>let's go</motion.button>
+                            <motion.button className="warning-popup__button" variants={popUpItemVariants} onClick={warningPopUpSubmitHandler} autoFocus>let's go</motion.button>
                         </motion.div>
-                    </PopupPage>
+                    </motion.section>
                 }
             </AnimatePresence>
         </>
     );
 };
 
-const PopupPage = styled(motion.section)`
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    background-color: #000000aa;
-    z-index: 9;
-    position: absolute;
-    inset: 0 0 0 0;
-    backdrop-filter: blur(15px) saturate(100%);
-    -webkit-backdrop-filter: blur(15px) saturate(100%);
+// const PopupPage = styled(motion.section)`
+//     width: 100vw;
+//     height: 100vh;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+//     background-color: #000000aa;
+//     z-index: 9;
+//     position: absolute;
+//     inset: 0 0 0 0;
+//     backdrop-filter: blur(15px) saturate(100%);
+//     -webkit-backdrop-filter: blur(15px) saturate(100%);
 
-    .popup-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        width: fit-content;
-        height: 50%;
-        max-width: 70%;
-        max-height: 50%;
-        padding: 2rem;
-        text-align: center;
-    }
+//     .popup-container {
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//         flex-direction: column;
+//         width: fit-content;
+//         height: 50%;
+//         max-width: 70%;
+//         max-height: 50%;
+//         padding: 2rem;
+//         text-align: center;
+//     }
 
-    .popup-title {
-        text-transform: uppercase;
-        letter-spacing: -2px;
-        word-spacing: 6px;
-    }
+//     .popup-title {
+//         text-transform: uppercase;
+//         letter-spacing: -2px;
+//         word-spacing: 6px;
+//     }
 
-    .popup-warning {
-        margin: 1rem;
-        color: #ff0000;
-        font-weight: 400;
-    }
+//     .popup-warning {
+//         margin: 1rem;
+//         color: #ff0000;
+//         font-weight: 400;
+//     }
 
-    .popup-text {
-        word-spacing: 2px;
-        font-weight: 400;
-        font-size: .8rem;
-        color: #ccc;
-        margin: .7rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+//     .popup-text {
+//         word-spacing: 2px;
+//         font-weight: 400;
+//         font-size: .8rem;
+//         color: #ccc;
+//         margin: .7rem;
+//         display: flex;
+//         justify-content: center;
+//         align-items: center;
+//     }
 
-    .popup-button {
-        margin-top: 1rem;
-        font-size: 1.2rem;
-        font-weight: 600;
-        border: none;
-        cursor: pointer;
-        text-transform: uppercase;
-        border-radius: 50px;
-        font-family: 'Outfit', sans-serif;
-        background-color: #ffffff08;
-        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-        width: 20rem;
-        height: 2.5rem;
-        transition: background .3s;
+//     .popup-button {
+//         margin-top: 1rem;
+//         font-size: 1.2rem;
+//         font-weight: 600;
+//         border: none;
+//         cursor: pointer;
+//         text-transform: uppercase;
+//         border-radius: 50px;
+//         font-family: 'Outfit', sans-serif;
+//         background-color: #ffffff08;
+//         box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+//         width: 20rem;
+//         height: 2.5rem;
+//         transition: background .3s;
 
-        &:hover {
-            background-color: #ffffff15;
-        }
+//         &:hover {
+//             background-color: #ffffff15;
+//         }
 
-        &:active {
-            background-color: #ffffff22;
-        }
-    }
+//         &:active {
+//             background-color: #ffffff22;
+//         }
+//     }
 
-    @media (max-width: 900px) {
-        .popup-container {
-            max-width: 90%;
-            max-height: 40%;
-            padding: .5rem;
-        }
+//     @media (max-width: 900px) {
+//         .popup-container {
+//             max-width: 90%;
+//             max-height: 40%;
+//             padding: .5rem;
+//         }
 
-        .popup-title {
-            font-size: 1.2rem;
-        }
+//         .popup-title {
+//             font-size: 1.2rem;
+//         }
 
-        .popup-warning {
-            font-size: .8rem;
-        }
+//         .popup-warning {
+//             font-size: .8rem;
+//         }
 
-        .popup-text {
-            font-size: .6rem;
-        }
-    }
-`;
+//         .popup-text {
+//             font-size: .6rem;
+//         }
+//     }
+// `;
 
 export default WarningPopup;
