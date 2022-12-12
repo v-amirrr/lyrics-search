d21d4b9b5bf7b6feb1341dea2da547548b4680ae
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-12 22:16:59 +0330

    feat: changed warning popup enter and exit animation

--- src/components/WarningPopup.js
+++ src/components/WarningPopup.js
@@ -5,53 +5,53 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 const popUpPageVariants = {
     hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 0.2, type: 'tween', when: "beforeChildren", childrenDelay: 0.2 } },
+    visible: { opacity: 1, transition: { duration: 0.2, type: 'tween', when: "beforeChildren" } },
     exit: { opacity: 0, transition: { duration: 0.2, type: 'tween', when: "afterChildren" } }
 };
 
 const popUpContainerVariants = {
     visible: { transition: { staggerChildren: 0.1 } },
-    exit: { transition: { staggerChildren: 0.05 } }
+    exit: { transition: { staggerChildren: 0.1 } }
 };
 
 const popUpItemVariants = {
-    hidden: { opacity: 0, y: -50, scale: 0.5 },
-    visible: { opacity: 1, y: 0, scale: 1, transition: { duration: 0.4, type: 'tween' } },
-    exit: { opacity: 0, y: 20, scale: 0.8, transition: { duration: 0.3, type: 'tween' } }
+    hidden: { opacity: 0, y: -20, scale: 0.8 },
+    visible: { opacity: 1, y: [-20, 20, 0], scale: 1, transition: { duration: 0.4, type: 'tween' } },
+    exit: { opacity: 0, y: 20, scaleX: 0, transition: { duration: 0.4, type: 'tween' } }
 };
 
 const WarningPopup = () => {
 
-    const [vpn, setVpn] = useState(false);
+    const [warningPopup, setWarningPopup] = useState(false);
 
     useEffect(() => {
-        if (!!sessionStorage.getItem("vpn") == true) {
-            setVpn(false);
+        if (!!sessionStorage.getItem("warning-popup") == true) {
+            setWarningPopup(false);
         } else {
-            setVpn(true);
+            setWarningPopup(true);
         }
     }, []);
     
-    const vpnPopUpSubmitHandler = e => {
+    const warningPopUpSubmitHandler = e => {
         e.preventDefault();
-        setVpn(false);
-        sessionStorage.setItem("vpn", "true");
+        setWarningPopup(false);
+        sessionStorage.setItem("warning-popup", "true");
     };
     
     return (
         <>
             <AnimatePresence>
                 {
-                    vpn
+                    warningPopup
                     &&
-                    <PopupPage initial='hidden' animate='visible' exit='exit' variants={popUpPageVariants} onKeyUp={e => e.key === "Enter" && vpnPopUpSubmitHandler}>
+                    <PopupPage initial='hidden' animate='visible' exit='exit' variants={popUpPageVariants} onKeyUp={e => e.key === "Enter" && warningPopUpSubmitHandler}>
                         <motion.div className="popup-container" variants={popUpContainerVariants}>
                             <motion.h1 variants={popUpItemVariants} className="popup-title">things you need to know</motion.h1>
                             <motion.p variants={popUpItemVariants} className="popup-warning">If you're in sanctioned countries like <b>Iran</b>, you have to turn on your <b>VPN</b> for using the app.</motion.p>
                             <motion.p variants={popUpItemVariants} className="popup-text">
                                 In this app you can search for any song you want and then if you click on them, you'll see more information about that song, such as artist, album, genre, lyrics and etc.
                             </motion.p>
-                            <motion.button autoFocus variants={popUpItemVariants} type="submit" className="popup-button" onClick={vpnPopUpSubmitHandler}>let's go</motion.button>
+                            <motion.button autoFocus variants={popUpItemVariants} type="submit" className="popup-button" onClick={warningPopUpSubmitHandler}>let's go</motion.button>
                         </motion.div>
                     </PopupPage>
                 }
