c9b366645414b703010af9f60bf4c58312d224fc
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-14 20:30:06 +0330

    perf: used BEM for naming classes and replaced Styled-Components with SCSS in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect, useRef } from 'react';
-
-import Songs from './Songs';
-
-import { useGetTrackQuery } from '../redux/apiSlice';
+import React, { useState, useRef } from 'react';
 
 import { IoClose } from 'react-icons/io5';
 
-import styled from 'styled-components';
+import "./Search.scss";
 import { motion } from 'framer-motion';
 
 const searchVariants = {
@@ -18,120 +14,33 @@ const searchVariants = {
 const Search = () => {
 
     const [searchInput, setSearchInput] = useState("");
-    const [searchButtonClicked , setSearchButtonClicked] = useState(false);
 
-    const { data, isLoading } = useGetTrackQuery(searchInput, { 
-        skip: !searchButtonClicked,
-        selectFromResult: ({ data }) => ({
-            data: data?.message?.body?.track_list,
-        }),
-    });
-    
-    const searchButtonHandler = e => {
-        e.preventDefault();
-        setSearchButtonClicked(true);
-    };
+    const searchInputRef = useRef();
 
     const clearButtonHandler = () => {
         setSearchInput("");
         searchInputRef.current.focus();
     };
 
-    useEffect(() => {
-        setSearchButtonClicked(false);
-    }, [data]);
-
-    const searchInputRef = useRef();
-
     return (
         <>
-            <SearchContainer initial='hidden' animate='visible' exit='exit' variants={searchVariants} data={data || isLoading || searchButtonClicked ? 1 : 0} clearbuttonshow={searchInput ? 1 : 0}>
-                <form className='search-form'>
-                    <input ref={searchInputRef} type="text" value={searchInput} onChange={e => setSearchInput(e.target.value)} placeholder="Search..." autoFocus />
-                    <i className='search-form-clear-icon' onClick={clearButtonHandler}><IoClose /></i>
-                    <button type='submit' onClick={searchButtonHandler}>search</button>
+            <motion.div className='search' initial='hidden' animate='visible' exit='exit' variants={searchVariants}>
+                <form className='search__form'>
+                    <input
+                        className='search__form__input'
+                        type="text" 
+                        value={searchInput} 
+                        onChange={e => setSearchInput(e.target.value)} 
+                        ref={searchInputRef} 
+                        placeholder="Search Song..." 
+                        autoFocus
+                    />
+                    <i className={searchInput ? 'search__form__clear-icon--show' : 'search__form__clear-icon--hide'} onClick={clearButtonHandler}><IoClose /></i>
+                    <button className='search__form__submit-btn' type='submit'>search</button>
                 </form>
-            </SearchContainer>
-
-            {
-                data
-                &&
-                <Songs data={data} isLoading={isLoading} searchButtonClicked={searchButtonClicked} />
-            }
+            </motion.div>
         </>
     );
 };
-
-const SearchContainer = styled(motion.div)`
-    width: 80%;
-    height: ${props => props.data ? "20%" : "100%"};
-    margin-top: 2rem;
-    padding: 2rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    transition: height .3s;
-
-    .search-form {
-        position: relative;
-        margin: 1rem;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50px;
-        font-family: 'Outfit', sans-serif;
-        background-color: #ffffff08;
-        box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-        width: 20rem;
-        height: 2.5rem;
-
-        input {
-            all: unset;
-            padding: 0 1.5rem 0 .7rem;
-            font-size: 1rem;
-            font-weight: 200;
-        }
-        
-        .search-form-clear-icon {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: absolute;
-            right: 5.2rem;
-            transform: ${props => props.clearbuttonshow ? "scale(1)" : "scale(0)"};
-            border-radius: 50%;
-            font-size: 1.3rem;
-            cursor: pointer;
-            transition: transform .2s;
-        }
-
-        button {
-            all: unset;
-            border-left: 1.3px solid #ffffff22;
-            border-radius: 0 50px 50px 0;
-            cursor: pointer;
-            user-select: none;
-            text-transform: uppercase;
-            letter-spacing: -1px;
-            font-weight: 600;
-            font-family: 'Outfit', sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            transition: background .2s;
-            height: 2.5rem;
-            width: 5rem;
-
-            &:hover {
-                background-color: #ffffff15;
-            }
-
-            &:active {
-                background-color: #ffffff22;
-            }
-        }
-    }
-`;
 
 export default Search;
