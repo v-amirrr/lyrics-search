4a3829809a4a16aefa334b7da180b36e11545969
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-17 16:09:10 +0330

    feat: added error section to the songs component

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -15,6 +15,12 @@ const songsContainerVariants = {
     exit: { opacity: 0, transition: { duration: 0.2, type: 'tween', when: "afterChildren" } }
 };
 
+const errorVariants = {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: [0.8, 1.1, 1], transition: { duration: 0.3, type: 'tween', staggerChildren: 0.1 } },
+    exit: { opacity: 0, scale: 0.8, transition: { duration: 0.3, type: 'tween', when: "beforeChildren" } }
+};
+
 const songsVariants = {
     visible: { transition: { staggerChildren: 0.1 } },
     exit: { opacity: 0, scale: 0.8, transition: { duration: 0.3, type: 'tween', when: "beforeChildren" } }
@@ -31,19 +37,13 @@ const Songs = () => {
 
     const searchInput = useSelector(state => state.searchStore.searchInputText);
 
-    const { data: songsList, loading } = useGetTrackQuery(searchInput, {
-        selectFromResult: ({ data }) => ({
-            data: data?.message?.body?.track_list,
-        }),
-    });
+    const { data: songsList, isLoading, isFetching, isError } = useGetTrackQuery(searchInput);
 
     useEffect(() => {
         if (searchInput) {
             setLoaderShow(true);
-            if (!loading) {
-                setTimeout(() => {
-                    setLoaderShow(false);
-                }, 500);
+            if (!isLoading) {
+                setLoaderShow(false);
             }
         }
     }, [searchInput]);
@@ -53,13 +53,20 @@ const Songs = () => {
             <motion.section className='songs-section' initial='hidden' animate='visible' exit='exit' variants={songsContainerVariants}>
                     <AnimatePresence exitBeforeEnter>
                         {
-                            loaderShow
+                            isLoading && !isError
                             ?
-                            <Loader key="loader" />
+                            <Loader key="loader"/>
                             :
-                            <motion.div key="songs" className='songs-section__songs-container' initial='hidden' animate='visible' exit='exit' variants={songsVariants}>
-                                {
-                                    songsList?.map(item => (
+                                isError
+                                ?
+                                <motion.div key="error" className='songs-section__error-container' initial='hidden' animate='visible' exit='exit' variants={errorVariants}>
+                                    <p className='songs-section__error-container__icon'>╯︿╰</p>
+                                    <p className='songs-section__error-container__error'>Looks like we have a problem.<br />Please check you connection status and try again.</p>
+                                    <p className='songs-section__error-container__warning'>If you're in sanctioned countries like <b>Iran</b>, you have to turn on your <b>VPN</b> for using the app. If you're alredy using a VPN, clearly it's not working so please use another one.<br />{":("}</p>
+                                </motion.div>
+                                :
+                                <motion.div key="songs" className='songs-section__songs-container' initial='hidden' animate='visible' exit='exit' variants={songsVariants}>
+                                    {songsList?.message?.body?.track_list?.map(item => (
                                         <Link to={`/${item.track.track_id}`} key={item.track.track_id}>
                                             <motion.div className='songs-section__songs-container__song' variants={songVariants} whileTap={{ scale: 0.8 }}>
                                                 <p className='songs-section__songs-container__song__name'>{item.track.track_name}</p>
@@ -68,9 +75,8 @@ const Songs = () => {
                                                 <div className='songs-section__songs-container__song__click-text'><p>Click For More Details</p></div>
                                             </motion.div>
                                         </Link>
-                                    ))
-                                }
-                            </motion.div>
+                                    ))}
+                                </motion.div>
                         }
                     </AnimatePresence>
             </motion.section>
