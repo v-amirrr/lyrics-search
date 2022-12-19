c9490e7eb80fb971bd247c69da2ebb1642505f67
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-19 21:34:14 +0330

    fix: fixed loader bug which happend when user searches for second time

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -37,47 +37,51 @@ const Songs = () => {
 
     const searchInput = useSelector(state => state.searchStore.searchInputText);
 
-    const { data: songsList, isLoading, isFetching, isError } = useGetTrackQuery(searchInput);
+    const { data: songsList, isLoading, isError } = useGetTrackQuery(searchInput);
 
     useEffect(() => {
-        if (searchInput) {
-            setLoaderShow(true);
-            if (!isLoading) {
+        setLoaderShow(true);
+        if (!isLoading) {
+            setTimeout(() => {
                 setLoaderShow(false);
-            }
+            }, 800);
         }
     }, [searchInput]);
+
+    useEffect(() => {
+        if (!isLoading) {
+            setLoaderShow(false);
+        }
+    }, [isLoading]);
 
     return (
         <>
             <motion.section className='songs-section' initial='hidden' animate='visible' exit='exit' variants={songsContainerVariants}>
                     <AnimatePresence exitBeforeEnter>
-                        {
-                            isLoading && !isError
+                        {loaderShow && !isError
+                        ?
+                        <Loader key="loader"/>
+                        :
+                            isError
                             ?
-                            <Loader key="loader"/>
+                            <motion.div key="error" className='songs-section__error-container' initial='hidden' animate='visible' exit='exit' variants={errorVariants}>
+                                <p className='songs-section__error-container__icon'>╯︿╰</p>
+                                <p className='songs-section__error-container__error'>Looks like we have a problem.<br />Please check you connection status and try again.</p>
+                                <p className='songs-section__error-container__warning'>If you're in sanctioned countries like <b>Iran</b>, you have to turn on your <b>VPN</b> for using the app. If you're alredy using a VPN, clearly it's not working so please use another one.<br />{":("}</p>
+                            </motion.div>
                             :
-                                isError
-                                ?
-                                <motion.div key="error" className='songs-section__error-container' initial='hidden' animate='visible' exit='exit' variants={errorVariants}>
-                                    <p className='songs-section__error-container__icon'>╯︿╰</p>
-                                    <p className='songs-section__error-container__error'>Looks like we have a problem.<br />Please check you connection status and try again.</p>
-                                    <p className='songs-section__error-container__warning'>If you're in sanctioned countries like <b>Iran</b>, you have to turn on your <b>VPN</b> for using the app. If you're alredy using a VPN, clearly it's not working so please use another one.<br />{":("}</p>
-                                </motion.div>
-                                :
-                                <motion.div key="songs" className='songs-section__songs-container' initial='hidden' animate='visible' exit='exit' variants={songsVariants}>
-                                    {songsList?.message?.body?.track_list?.map(item => (
-                                        <Link to={`/${item.track.track_id}`} key={item.track.track_id}>
-                                            <motion.div className='songs-section__songs-container__song' variants={songVariants} whileTap={{ scale: 0.8 }}>
-                                                <p className='songs-section__songs-container__song__name'>{item.track.track_name}</p>
-                                                <p className='songs-section__songs-container__song__artist'>{item.track.artist_name}</p>
-                                                <p className='songs-section__songs-container__song__genre'>{item?.track?.primary_genres?.music_genre_list[0]?.music_genre?.music_genre_name}</p>
-                                                <div className='songs-section__songs-container__song__click-text'><p>Click For More Details</p></div>
-                                            </motion.div>
-                                        </Link>
-                                    ))}
-                                </motion.div>
-                        }
+                            <motion.div key="songs" className='songs-section__songs-container' initial='hidden' animate='visible' exit='exit' variants={songsVariants}>
+                                {songsList?.message?.body?.track_list?.map(item => (
+                                    <Link to={`/${item.track.track_id}`} key={item.track.track_id}>
+                                        <motion.div className='songs-section__songs-container__song' variants={songVariants} whileTap={{ scale: 0.8 }}>
+                                            <p className='songs-section__songs-container__song__name'>{item.track.track_name}</p>
+                                            <p className='songs-section__songs-container__song__artist'>{item.track.artist_name}</p>
+                                            <p className='songs-section__songs-container__song__genre'>{item?.track?.primary_genres?.music_genre_list[0]?.music_genre?.music_genre_name}</p>
+                                            <div className='songs-section__songs-container__song__click-text'><p>Click For More Details</p></div>
+                                        </motion.div>
+                                    </Link>
+                                ))}
+                            </motion.div>}
                     </AnimatePresence>
             </motion.section>
         </>
