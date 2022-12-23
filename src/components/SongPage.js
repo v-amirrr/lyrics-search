3c712d56a335c909034c828ae69e49c67822ce46
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-23 15:03:11 +0330

    feat: added the loader to the song page

--- src/components/SongPage.js
+++ src/components/SongPage.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useLocation, useParams, Link } from 'react-router-dom';
 
 import { useGetLyricsQuery, useGetTrackQuery } from '../redux/apiSlice';
 
+import Loader from './Loader';
+
 import { IoMdArrowRoundBack } from 'react-icons/io';
 import { IoChevronDown } from 'react-icons/io5';
 import "./SongPage.scss";
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const songPageVariants = {
     hidden: { opacity: 0, y: -50, scale: 0.9 },
@@ -20,61 +22,81 @@ const SongPage = () => {
     const { id } = useParams();
     const location = useLocation();
 
-    const { data: trackData } = useGetTrackQuery(id);
+    const { data: trackData, isLoading: isLoadingTrack, isError: isErrorTrack } = useGetTrackQuery(id);
 
-    const { data: trackLyrics } = useGetLyricsQuery(id, {
+    const { data: trackLyrics, isLoading: isLoadingLyrics, isError: isErrorLyrics } = useGetLyricsQuery(id, {
         skip: !location.state
     });
 
     const [inputCheckbox, setInputCheckbox] = useState(false);
+    const [loaderShow, setLoaderShow] = useState(true);
+
+    useEffect(() => {
+        setLoaderShow(true);
+        if (!isLoadingTrack && !isLoadingLyrics) {
+            setTimeout(() => {
+                setLoaderShow(false);
+            }, 800);
+        }
+    }, [id]);
+
+    useEffect(() => {
+        if (!isLoadingTrack && !isLoadingLyrics) {
+            setLoaderShow(false);
+        }
+    }, [isLoadingTrack, isLoadingLyrics]);
 
     return (
         <>
             <motion.section className='song-page' initial='hidden' animate='visible' exit='exit' variants={songPageVariants}>
-                <div className='song-page__song-container'>
-                    <div className='song-page__song-container__song-data'>
-                        <p className='song-page__song-container__song-data__track'>
-                            {trackData?.message?.body?.track?.track_name}
-                        </p>
-                        <p className='song-page__song-container__song-data__artist'>
-                            {trackData?.message?.body?.track?.artist_name}
-                        </p>
-                        <p className='song-page__song-container__song-data__album'>
-                            {trackData?.message?.body?.track?.album_name}
-                        </p>
-                        <p className='song-page__song-container__song-data__genre'>
-                            {trackData?.message?.body?.track?.primary_genres?.music_genre_list[0]?.music_genre?.music_genre_name}
-                        </p>
-                        <div className={inputCheckbox ? 'song-page__song-container__song-data__lyrics--open' : 'song-page__song-container__song-data__lyrics--close'}>
-                            <div>
-                                Song Lyrics
-                                <input type="checkbox" onChange={() => setInputCheckbox(!inputCheckbox)} checked={inputCheckbox}/>
-                                <i><IoChevronDown /></i>
-                            </div>
-                            <div>
-                                {trackData?.message?.body?.track?.has_lyrics ?
-                                trackLyrics?.message?.body?.lyrics?.lyrics_body :
-                                "There is no lyrics avaible for this song."}
+                <AnimatePresence exitBeforeEnter>
+                    {loaderShow ?
+                    <Loader key="loader-song-page" /> :
+                    <div className='song-page__song-container'>
+                        <div className='song-page__song-container__song-data'>
+                            <p className='song-page__song-container__song-data__track'>
+                                {trackData?.message?.body?.track?.track_name}
+                            </p>
+                            <p className='song-page__song-container__song-data__artist'>
+                                {trackData?.message?.body?.track?.artist_name}
+                            </p>
+                            <p className='song-page__song-container__song-data__album'>
+                                {trackData?.message?.body?.track?.album_name}
+                            </p>
+                            <p className='song-page__song-container__song-data__genre'>
+                                {trackData?.message?.body?.track?.primary_genres?.music_genre_list[0]?.music_genre?.music_genre_name}
+                            </p>
+                            <div className={inputCheckbox ? 'song-page__song-container__song-data__lyrics--open' : 'song-page__song-container__song-data__lyrics--close'}>
+                                <div>
+                                    Song Lyrics
+                                    <input type="checkbox" onChange={() => setInputCheckbox(!inputCheckbox)} checked={inputCheckbox}/>
+                                    <i><IoChevronDown /></i>
+                                </div>
+                                <div>
+                                    {trackData?.message?.body?.track?.has_lyrics ?
+                                    trackLyrics?.message?.body?.lyrics?.lyrics_body :
+                                    "There is no lyrics avaible for this song."}
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <a className="song-page__song-container__song-link link" href={trackData?.message?.body?.track?.track_share_url} target="_blank" rel="noopener noreferror">
-                        Musixmatch Link
-                    </a>
+                        <a className="song-page__song-container__song-link link" href={trackData?.message?.body?.track?.track_share_url} target="_blank" rel="noopener noreferror">
+                            Musixmatch Link
+                        </a>
 
-                    <div className='song-page__song-container__icons'>
-                        {trackData?.message?.body?.track?.explicit ? <span>E</span> : ""}
-                        {trackData?.message?.body?.track?.has_lyrics ? <span>L</span> : ""}
-                    </div>
+                        <div className='song-page__song-container__icons'>
+                            {trackData?.message?.body?.track?.explicit ? <span>E</span> : ""}
+                            {trackData?.message?.body?.track?.has_lyrics ? <span>L</span> : ""}
+                        </div>
 
-                    <Link to="/">
-                        <motion.div className='song-page__song-container__back-btn' whileTap={{ scale: 0.8 }}>
-                            <i><IoMdArrowRoundBack /></i>
-                            <p>Back To Home</p>
-                        </motion.div>
-                    </Link>
-                </div>
+                        <Link to="/">
+                            <motion.div className='song-page__song-container__back-btn' whileTap={{ scale: 0.8 }}>
+                                <i><IoMdArrowRoundBack /></i>
+                                <p>Back To Home</p>
+                            </motion.div>
+                        </Link>
+                    </div>}
+                </AnimatePresence>
             </motion.section>
         </>
     );
