a3d2c2ffc1f0663fc3d9c8e9a714fd75c45a0381
Amir Valizadeh <79039612+v-amirrr@users.noreply.github.com>
2022-12-25 22:17:35 +0330

    feat: added nothing was found to the Songs component

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -16,7 +16,7 @@ const songsContainerVariants = {
     exit: { opacity: 0, transition: { duration: 0.2, type: 'tween', when: "afterChildren" } }
 };
 
-const errorVariants = {
+const nothingVariants = {
     hidden: { opacity: 0, scale: 0.8 },
     visible: { opacity: 1, scale: [0.8, 1.05, 1], transition: { duration: 0.4, type: 'tween' } },
     exit: { opacity: 0, scale: 0.8, transition: { duration: 0.4, type: 'tween' } }
@@ -35,6 +35,7 @@ const songVariants = {
 const Songs = () => {
 
     const [loaderShow, setLoaderShow] = useState(true);
+    const [nothingWasFound, setNothingWasFound] = useState(false);
 
     const searchInput = useSelector(state => state.searchStore.searchInputText);
 
@@ -45,6 +46,9 @@ const Songs = () => {
         if (!isLoading) {
             setTimeout(() => {
                 setLoaderShow(false);
+                if (songsList?.message?.body?.track_list.length == 0) {
+                    setNothingWasFound(true);
+                }
             }, 800);
         }
     }, [searchInput]);
@@ -52,6 +56,9 @@ const Songs = () => {
     useEffect(() => {
         if (!isLoading) {
             setLoaderShow(false);
+            if (songsList?.message?.body?.track_list.length == 0) {
+                setNothingWasFound(true);
+            }
         }
     }, [isLoading]);
 
@@ -63,22 +70,26 @@ const Songs = () => {
                         <Loader key="loader-songs"/> :
                             isError ?
                             <Error key="error-songs" /> :
-                            <motion.div key="songs" className='songs-section__songs-container' initial='hidden' animate='visible' exit='exit' variants={songsVariants}>
-                                {songsList?.message?.body?.track_list?.map(item => (
-                                    <Link to={`/${item.track.commontrack_id}`} key={item.track.commontrack_id} state={item.track.has_lyrics}>
-                                        <motion.div className='songs-section__songs-container__song' variants={songVariants} whileTap={{ scale: 0.8 }}>
-                                            <div className='songs-section__songs-container__song__icons'>
-                                                {item.track.explicit ? <span>E</span> : ""}
-                                                {item.track.has_lyrics ? <span>L</span> : ""}
-                                            </div>
-                                            <p className='songs-section__songs-container__song__name'>{item.track.track_name}</p>
-                                            <p className='songs-section__songs-container__song__artist'>{item.track.artist_name}</p>
-                                            <p className='songs-section__songs-container__song__genre'>{item.track.primary_genres?.music_genre_list[0]?.music_genre?.music_genre_name}</p>
-                                            <div className='songs-section__songs-container__song__click-text'><p>Click For More Details</p></div>
-                                        </motion.div>
-                                    </Link>
-                                ))}
-                            </motion.div>}
+                                nothingWasFound ?
+                                <motion.div key="nothing-was-found" initial='hidden' animate='visible' exit='exit' variants={nothingVariants}>
+                                    NOTHING WAS FOUND
+                                </motion.div> :
+                                <motion.div key="songs" className='songs-section__songs-container' initial='hidden' animate='visible' exit='exit' variants={songsVariants}>
+                                    {songsList?.message?.body?.track_list?.map(item => (
+                                        <Link to={`/${item.track.commontrack_id}`} key={item.track.commontrack_id} state={item.track.has_lyrics}>
+                                            <motion.div className='songs-section__songs-container__song' variants={songVariants} whileTap={{ scale: 0.8 }}>
+                                                <div className='songs-section__songs-container__song__icons'>
+                                                    {item.track.explicit ? <span>E</span> : ""}
+                                                    {item.track.has_lyrics ? <span>L</span> : ""}
+                                                </div>
+                                                <p className='songs-section__songs-container__song__name'>{item.track.track_name}</p>
+                                                <p className='songs-section__songs-container__song__artist'>{item.track.artist_name}</p>
+                                                <p className='songs-section__songs-container__song__genre'>{item.track.primary_genres?.music_genre_list[0]?.music_genre?.music_genre_name}</p>
+                                                <div className='songs-section__songs-container__song__click-text'><p>Click For More Details</p></div>
+                                            </motion.div>
+                                        </Link>
+                                    ))}
+                                </motion.div>}
                     </AnimatePresence>
             </motion.section>
         </>
